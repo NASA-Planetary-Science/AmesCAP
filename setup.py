e81613fadcfc2107994a98803416fe4d04e18ea7
akling <akling@Leo.local>
2020-03-19 15:39:34 -0700

    Creating the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='amesgcm',
+      version='0.1',
+      description='Analysis pipeline for the NASA Ames MGCM',
+      url='http://github.com/alex-kling/amesgcm',
+      author='Mars Climate Modeling Center',
+      author_email='alexandre.m.kling@nasa.gov',
+      license='TBD',
+      packages=['amesgcm'],
+      zip_safe=False)
