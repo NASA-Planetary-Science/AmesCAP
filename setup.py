8ce5d1689f41d860d2406ca1abad46e7c93b3f3e
akling <akling@Leo.local>
2020-03-31 16:21:36 -0700

    updated structure  again

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='amesgcm',
       license='TBD',
       scripts=['bin/MarsPull.py','bin/MarsDocumentation.sh','bin/MarsPlot.py','bin/MarsVars.py','bin/MarsFiles.py'],
       install_requires=['requests','netCDF4','numpy','matplotlib'],
-      packages=['lib'],
+      packages=find_packages(where='lib'),
       package_dir={'': 'lib',},
       zip_safe=False)
