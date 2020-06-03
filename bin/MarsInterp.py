60ebd97a73c8ba196153aed1a61c3165c1597c75
akling <akling@Leo.ndc.nasa.gov>
2020-06-02 20:42:16 -0700

    Updated MarsInterp to support diurn files

--- bin/MarsInterp.py
+++ bin/MarsInterp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 #Load generic Python Modules 
-import argparse #parse arguments
-import os       #access operating systems function
-import subprocess #run command
-import sys       #system command
-import time
-
+import argparse   # parse arguments
+import os         # access operating systems function
+import subprocess # run command
+import sys        # system command
+import time       # monitor interpolation time
+import re         # string matching module to handle time_of_day_XX
 
 from amesgcm.FV3_utils import fms_press_calc,fms_Z_calc,vinterp,find_n
 from amesgcm.Script_utils import check_file_tape,prYellow,prRed,prCyan,prGreen,prPurple, print_fileContent
@@ -70,29 +70,6 @@ M_co2 =0.044 # kg/mol
 #===========================
 filepath=os.getcwd()
 
-
-    
-def compute_p_3D(ps,ak,bk,temp):
-    dim_out=temp.shape
-    p_3D= fms_press_calc(ps,ak,bk,lev_type='full')
-    if len(p_3D.shape)==4:p_3D=p_3D.transpose([0,3,1,2])# p_3D [tim,lat,lon,lev] ->[tim, lev, lat, lon]
-    if len(p_3D.shape)==3:p_3D=p_3D.transpose([2,0,1]) #p_3D [lat,lon,lev] ->    [lev, lat, lon]
-    return p_3D.reshape(dim_out)
-
-
-def compute_zfull(ps,ak,bk,temp):
-    """
-    Compute the altitude AGL in m
-    """
-    dim_out=temp.shape
-    zfull=fms_Z_calc(ps,ak,bk,temp.transpose([0,2,3,1]),topo=0.,lev_type='full') #transpose temp like PRESS_h
-    if len(zfull.shape)==4:
-        zfull=fms_Z_calc(ps,ak,bk,temp.transpose([0,2,3,1]),topo=0.,lev_type='full') #transpose temp like PRESS_h
-        zfull=zfull.transpose([0,3,1,2])# p_3D [tim,lat,lon,lev] ->[tim, lev, lat, lon]
-    if len(zfull.shape)==3:
-        zfull=fms_Z_calc(ps,ak,bk,temp.transpose([1,2,0]),topo=0.,lev_type='full') #transpose temp like PRESS_h
-        zfull=zfull.transpose([2,0,1]) #p_3D [lat,lon,lev] ->    [lev, lat, lon]
-    return zfull.reshape(dim_out)
 
 
 def main():
@@ -185,32 +162,41 @@ def main():
         #=================================================================
 
         fNcdf=Dataset(ifile, 'r', format='NETCDF4_CLASSIC')
-        # Load pk and bk and ps for 3D pressure field calculation.
-        
+        # Load pk and bk and ps for 3D pressure field calculation. 
+        # We will read the pk and bk for each file in case the vertical resolution is changed.
+         
         try:
+            #First try to read pk and bk in the file
             pk=np.array(fNcdf.variables['pk'])
             bk=np.array(fNcdf.variables['bk'])
         except:
-            pass    
+            #If pk and bk are not available in the file, try the matching XXXXX.fixed.nc
+            name_fixed=filepath+'/'+ifile[0:5]+'.fixed.nc'
+            f_fixed=Dataset(name_fixed, 'r', format='NETCDF4_CLASSIC')  
+            pk=np.array(f_fixed.variables['pk'])
+            bk=np.array(f_fixed.variables['bk'])  
+            f_fixed.close()
             
         ps=np.array(fNcdf.variables['ps'])
-        #Do this only once
-        if ifile==file_list[0]:
-            
-            pass
-        
-        if len(ps.shape)==4:    
-            prRed('Interpolation of atmos_diurn not yet supported')
-            exit()
-        else:
-            permut=[1,0,2,3] # Put vertical axis first, e.g (time,lev,lat,lon) >>> (lev,time,lat,lon)
-            
+
+        if len(ps.shape)==3:    
+            do_diurn=False
+            permut=[1,0,2,3] # Put vertical axis first for 4D variable, e.g (time,lev,lat,lon) >>> (lev,time,lat,lon)
+                             #                              ( 0    1   2   3 ) >>> ( 1   0    2   3 )
+        elif len(ps.shape)==4: 
+            do_diurn=True
+            #find time of day variable name
+            regex=re.compile('time_of_day.')
+            varset=fNcdf.variables.keys()
+            tod_name=[string for string in varset if re.match(regex, string)][0] #Exctract the 1st elemnt of the list
+            permut=[2,1,0,3,4]  #Same for diun files, e.g (time,time_of_day_XX,lev,lat,lon) >>> (lev,time_of_day_XX,time,lat,lon)
+                                #                         (  0        1         2   3   4)  >>> ( 2       1          0    3   4 )
         #== Compute levels in the file, these are permutted arrays
         
         # Suppress divided by zero error ==
         with np.errstate(divide='ignore', invalid='ignore'):  
             if interp_type=='pstd':
-                L_3D_P= fms_press_calc(ps,pk,bk,lev_type='full') #permuted by default
+                L_3D_P= fms_press_calc(ps,pk,bk,lev_type='full') #permuted by default, e.g lev is first
                 
             elif interp_type=='zagl':
                 temp=np.array(fNcdf.variables['temp'])
@@ -220,26 +206,37 @@ def main():
                 temp=np.array(fNcdf.variables['temp'])
                 #Expend the zsurf array to the time dimension
                 zflat=np.repeat(zsurf[np.newaxis,:],ps.shape[0],axis=0)     
+                if do_diurn:
+                    zflat=np.repeat(zflat[:,np.newaxis,:,:],ps.shape[1],axis=1)
+                    
                 L_3D_P= fms_Z_calc(ps,pk,bk,temp.transpose(permut),topo=zflat,lev_type='full') 
                 
             
-        
         fnew = Ncdf(newname,'Pressure interpolation using MarsInterp.py')
         #===========      Replicate existing DIMENSIONS but pfull  =================
+        #get all variables in file:
+        var_list=fNcdf.variables.keys()
+        
         fnew.copy_all_dims_from_Ncfile(fNcdf,exclude_dim=['pfull'])
         fnew.add_dim_with_content(interp_type,lev_in,longname_txt,units_txt) #Add new vertical dimension
+        
         
         fnew.copy_Ncaxis_with_content(fNcdf.variables['lon'])
         fnew.copy_Ncaxis_with_content(fNcdf.variables['lat'])
         fnew.copy_Ncaxis_with_content(fNcdf.variables['time'])
         
-        #get all variables in file:
-        var_list=fNcdf.variables.keys()
+        
+        if do_diurn:
+            #find time_of_day variable name
+            regex=re.compile('time_of_day.')
+            tod_name=[string for string in var_list if re.match(regex, string)][0]
+            fnew.copy_Ncaxis_with_content(fNcdf.variables[tod_name])
         
         #We will re-use the indices for each files, this speeds-up the calculation
         compute_indices=True    
         for ivar in var_list: 
-            if fNcdf.variables[ivar].dimensions==('time','pfull', 'lat', 'lon'):
+            if (fNcdf.variables[ivar].dimensions==('time','pfull', 'lat', 'lon') or
+             fNcdf.variables[ivar].dimensions==('time',tod_name,'pfull', 'lat', 'lon')):
                 if compute_indices:
                     prCyan("Computing indices ...")
                     index=find_n(L_3D_P,lev_in,reverse_input=need_to_reverse)
@@ -250,11 +247,18 @@ def main():
                 varIN=fNcdf.variables[ivar][:]
                 #==This with loop suppresses divided by zero error for the Legacy GCM==
                 with np.errstate(divide='ignore', invalid='ignore'):
-                    varOUT=vinterp(varIN.transpose(permut),L_3D_P ,lev_in,type=interp_technic,reverse_input=need_to_reverse,masktop=True,index=index).transpose(permut)
-                fnew.log_variable(ivar,varOUT,('time',interp_type, 'lat', 'lon'),fNcdf.variables[ivar].long_name,fNcdf.variables[ivar].units)
+                    varOUT=vinterp(varIN.transpose(permut),L_3D_P,     
+                                   lev_in,type=interp_technic,reverse_input=need_to_reverse,
+                                   masktop=True,index=index).transpose(permut)
+                if not do_diurn:                   
+                    fnew.log_variable(ivar,varOUT,('time',interp_type, 'lat', 'lon'),
+                                      fNcdf.variables[ivar].long_name,fNcdf.variables[ivar].units)
+                else:
+                    fnew.log_variable(ivar,varOUT,('time',tod_name,interp_type, 'lat', 'lon'),
+                                      fNcdf.variables[ivar].long_name,fNcdf.variables[ivar].units)                      
             else:
                 
-                if  ivar not in ['time','pfull', 'lat', 'lon','phalf','pk','bk','zstd','zagl']:
+                if  ivar not in ['time','pfull', 'lat', 'lon','phalf','pk','bk','pstd','zstd','zagl',tod_name]:
                     #print("\r Copying over: %s..."%(ivar), end='')
                     prCyan("Copying over: %s..."%(ivar))
                     fnew.copy_Ncvar(fNcdf.variables[ivar]) 
