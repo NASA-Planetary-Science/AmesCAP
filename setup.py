49e7b7d340b433d96bbdfb4ffdcfa2ee4bf25485
Court <42366074+falconstryker@users.noreply.github.com>
2022-02-23 14:22:47 -0800

    Changed setup.py for this repository
    Changed 'amesgcm' to 'amesgcmcb' to differentiate it from the original repository (at GitHub.com/alex-kling/amesgcm) so that both can be installed locally and separately for debugging.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='amesgcm',
+setup(name='amesgcmcb',
       version='0.2',
-      description='Analysis pipeline for the NASA Ames MGCM',
-      url='http://github.com/alex-kling/amesgcm',
+      description='Analysis pipeline for the NASA Ames MGCM - working repository',
+      url='http://github.com/falconstryker/amesgcm',
       author='Mars Climate Modeling Center',
-      author_email='alexandre.m.kling@nasa.gov',
+      author_email='cmlbatterson@gmail.com',
       license='TBD',
       scripts=['bin/MarsPull.py','bin/MarsInterp.py','bin/MarsPlot.py','bin/MarsVars.py','bin/MarsFiles.py','bin/MarsViewer.py'],
       install_requires=['requests','netCDF4','numpy','matplotlib','scipy'],
