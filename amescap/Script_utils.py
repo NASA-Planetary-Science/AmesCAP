984685a90a43cb6abf2239d2ec9bc85a38dbccc7
alex-kling <alexandre.kling@protonmail.com>
2023-11-02 10:06:22 -0700

    Updated script utils

--- amescap/Script_utils.py
+++ amescap/Script_utils.py
@@ -17,6 +17,17 @@ def prYellow(skk): print("\033[93m{}\033[00m".format(skk))
 def prPurple(skk): print("\033[95m{}\033[00m".format(skk))
 def prLightPurple(skk): print("\033[94m{}\033[00m".format(skk))
 
+
+Cyan = "\033[96m"
+Blue = "\033[94m"
+Yellow = "\033[93m"
+NoColor = "\033[00m"
+Red = "\033[91m"
+Green = "\033[92m"
+Purple = "\033[95m"
+
+
+
 def MY_func(Ls_cont):
     '''
     This function return the Mars Year
