7cd0158394775876ad69ca2ffdb3242b5c3381c3
Court <42366074+falconstryker@users.noreply.github.com>
2023-12-20 10:54:28 -0800

    troubleshooting readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,11 +69,10 @@ exclude_patterns = []
 
 # html_theme = 'furo'
 html_theme = "sphinx_rtd_theme"
-
 # Options: alabaster, classic, sphinxdoc, bizstyle, furo
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
