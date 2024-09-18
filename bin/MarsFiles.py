98c411d59b646d41f1a3c9a3d4bf67c602221783
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2024-09-18 15:04:18 -0700

    debugging split_files function, adding other dims to split

--- bin/MarsFiles.py
+++ bin/MarsFiles.py
@@ -545,12 +545,16 @@ def split_files(file_list, split_dim):
                 var_out = varNcf[..., ..., lower_bound:upper_bound, ...]
             elif split_dim == 'lat' and varNcf.ndim == 3:
                 var_out = varNcf[..., lower_bound:upper_bound, ...]
+            elif split_dim == 'lat' and varNcf.ndim == 2:
+                var_out = varNcf[lower_bound:upper_bound, ...]
             elif split_dim == 'lon' and varNcf.ndim == 5:
                 var_out = varNcf[..., ..., ..., ..., lower_bound:upper_bound]
             elif split_dim == 'lon' and varNcf.ndim == 4:
                 var_out = varNcf[..., ..., ..., lower_bound:upper_bound]
             elif split_dim == 'lon' and varNcf.ndim == 3:
                 var_out = varNcf[..., ..., lower_bound:upper_bound]
+            elif split_dim == 'lon' and varNcf.ndim == 2:
+                var_out = varNcf[lower_bound:upper_bound, ...]
             longname_txt, units_txt = get_longname_units(fNcdf, ivar)
             Log.log_variable(ivar, var_out, varNcf.dimensions,
                              longname_txt, units_txt)
