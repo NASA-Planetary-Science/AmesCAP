c9bb0bfd8dd45ee3bb0773ffd5c6b57e0ad4c036
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2024-12-09 14:31:23 -0800

    added pyproject.toml which will replace setup.py eventually, added .flake8 requirements and a dev.txt for developer requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
 from setuptools import setup
 
-setup(name = "amescap",
-      version = "0.3",
-      description = "Analysis pipeline for the NASA Ames MGCM",
-      url = "https://github.com/NASA-Planetary-Science/AmesCAP",
-      author = "Mars Climate Modeling Center",
-      author_email = "alexandre.m.kling@nasa.gov",
-      license = "MIT License",
-      scripts = ["bin/MarsPull.py", "bin/MarsInterp.py", 
-                 "bin/MarsPlot.py", "bin/MarsVars.py", 
-                 "bin/MarsFiles.py", "bin/MarsFormat.py", 
-                 "bin/MarsCalendar.py"],
-      install_requires = ["requests", "netCDF4", "numpy", "matplotlib", 
-                          "scipy", "xarray"],
-      packages = ["amescap"],
-      data_files = [
-            ("mars_data", ["mars_data/Legacy.fixed.nc"]),
-            ("mars_templates", ["mars_templates/legacy.in", 
-                                "mars_templates/amescap_profile"])
-            ],
-      include_package_data = True,
-      zip_safe = False)
+setup(
+    name="amescap",
+    version="0.3",
+    description="Analysis pipeline for the NASA Ames MGCM",
+    url="https://github.com/NASA-Planetary-Science/AmesCAP",
+    author="Mars Climate Modeling Center",
+    author_email="alexandre.m.kling@nasa.gov",
+    license="MIT License",
+    scripts=[
+        "bin/MarsPull.py",
+        "bin/MarsInterp.py",
+        "bin/MarsPlot.py",
+        "bin/MarsVars.py",
+        "bin/MarsFiles.py",
+        "bin/MarsFormat.py",
+        "bin/MarsCalendar.py"
+    ],
+    install_requires=[
+        "requests>=2.31.0",
+        "netCDF4>=1.6.5",
+        "numpy>=1.26.2",
+        "matplotlib>=3.8.2",
+        "scipy>=1.11.4",
+        "xarray>=2023.5.0",
+    ],
+    packages=["amescap"],
+    data_files=[
+        ("mars_data", ["mars_data/Legacy.fixed.nc"]),
+        ("mars_templates", [
+            "mars_templates/legacy.in",
+            "mars_templates/amescap_profile"
+        ])
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.7",
+)
