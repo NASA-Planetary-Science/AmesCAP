39d96d0294e1e48f5c325026b3e7e68e85e1800b
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2025-02-26 15:34:51 -0800

    adding mutually inclusive arguments

--- bin/MarsCalendar.py
+++ bin/MarsCalendar.py
@@ -43,11 +43,12 @@ parser = argparse.ArgumentParser(
 )
 
 group = parser.add_argument_group("Required Arguments", 
-                                  "MarsCalendar requires one of these"
+                                  "MarsCalendar requires one of these "
                                   "arguments.")
 exclusive_group = parser.add_mutually_exclusive_group(required=True)
 
 exclusive_group.add_argument('-sol', '--sol', nargs='+', type=float,
+    required=True,
     help=(
         f"Input sol number. Required. Can either be one sol or a"
         f"range with an increment ``[start stop step]``.\n"
@@ -59,6 +60,7 @@ exclusive_group.add_argument('-sol', '--sol', nargs='+', type=float,
 )
 
 exclusive_group.add_argument('-ls', '--ls', nargs='+', type=float,
+    required=True,
     help=(
         f"Return the sol number corresponding to this Ls.\n"
         f"{Green}Example:\n"
