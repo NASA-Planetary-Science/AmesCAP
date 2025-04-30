11ae1e14fa93f00437d9c321c79feb286e0034dd
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2025-04-29 17:06:56 -0700

    troubleshooting dp to dz

--- bin/MarsVars.py
+++ bin/MarsVars.py
@@ -2072,7 +2072,7 @@ def main():
                 # Ensure required variables (DP, DZ) exist
                 if not (check_variable_exists('DP', f.variables.keys()) and 
                         check_variable_exists('DZ', f.variables.keys())):
-                    print(f"{Red}Error: DP and DZ variables required for conversion. Add them with CAP:\n{cyan}MarsVars {ifile} -add DP DZ{Nclr}")
+                    print(f"{Red}Error: DP and DZ variables required for conversion. Add them with CAP:\n{Nclr}MarsVars {ifile} -add DP DZ")
                     f.close()
                     continue
                 
