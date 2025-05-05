defeb90cfb5ab82f0136d003a2936f98687743e3
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2025-05-05 15:52:45 -0700

    troubleshooting windows tests for marsvars

--- bin/MarsVars.py
+++ bin/MarsVars.py
@@ -181,7 +181,7 @@ master_list = {
         ['pfull']
     ],
     'scorer_wl': [
-        "Scorer horiz. λ = 2π/√(l^2)",
+        "Scorer horiz. lambda = 2π/√(l^2)",
         'm',
         ['ps', 'temp', 'ucomp'],
         ['pfull']
