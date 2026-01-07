00a91e8d0f57c27bcf21918dbb5faeedbfd5149e
Courtney ML Batterson <courtney.m.batterson@nasa.gov>
2026-01-07 15:11:54 -0800

    adding weighted mean to statistics

--- amescap/Script_utils.py
+++ amescap/Script_utils.py
@@ -206,8 +206,9 @@ def print_varContent(fileNcdf, list_varfull, print_stat=False):
                     Max = np.nanmax(var)
                     print(f"{Cyan}{varfull:>26s}|{Min:>15g}|{Mean:>15g}|"
                           f"{Max:>15g}|{Nclr}")
+                    print(f"{Cyan}______________________________________________________________________{Nclr}")
                     print(f"{Cyan}Global area-weighted mean "
-                          f"{varfull:>26s}: {Wmean}{Nclr}")
+                          f"{varfull}: {Wmean:>15g}{Nclr}")
 
                     if cmd_txt == "areo":
                         # If variable is areo then print modulo
